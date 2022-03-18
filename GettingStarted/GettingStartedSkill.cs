0d8f0c3b1915952d043d9ab281174cfaf55e56bf
Brad Irby <brad@bradirby.com>
2022-03-19 00:21:32 +0100

    Adding GettingStarted app

---
+++ GettingStarted/GettingStartedSkill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlexaNetCore;
+
+namespace AlexaNetCoreGettingStarted
+{
+    public class GettingStartedSkill : AlexaSkillBase
+    {
+        public GettingStartedSkill()
+        {
+            SetSkillVersion("0.1");
+            RegisterIntentHandler(new DefaultLaunchIntentHandler("I Love Alexa Net Core"));
+        }
+    }
+}
