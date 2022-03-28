5b818d581e5bb4946d4a856828c4c5da4bbbb60f
Brad Irby <brad@bradirby.com>
2022-03-28 10:27:59 +0200

    Updating for Hello World

--- GettingStarted/GettingStartedSkill.cs
+++ GettingStarted/GettingStartedSkill.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AlexaNetCore;
+using AlexaNetCore_GettingStarted.Intents;
 
-namespace AlexaNetCoreGettingStarted
+namespace AlexaNetCore_GettingStarted
 {
     public class GettingStartedSkill : AlexaSkillBase
     {
@@ -13,6 +14,13 @@ namespace AlexaNetCoreGettingStarted
         {
             SetSkillVersion("0.1");
             RegisterIntentHandler(new DefaultLaunchIntentHandler("I Love Alexa Net Core"));
+            RegisterIntentHandler(new DefaultStopIntentHandler("OK, stopping.  Thanks for visiting"));
+            RegisterIntentHandler(new DefaultCancelIntentHandler("OK, canceling.  Thanks for visiting"));
+            RegisterIntentHandler(new DefaultHelpIntentHandler("I haven't added the help yet"));
+            RegisterIntentHandler(new DefaultFallbackIntentHandler("Default intent asks if you can try that again"));
+
+            //register your custom intent handlers
+            RegisterIntentHandler(new NetCoreBirthDateIntentHandler());
         }
     }
 }
---
+++ GettingStarted/Intents/NetCoreBirthDateIntentHandler.cs
@@ -0,0 +1,30 @@
+﻿using AlexaNetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexaNetCore_GettingStarted.Intents
+{
+    public class NetCoreBirthDateIntentHandler : AlexaIntentHandlerBase
+    {
+        public NetCoreBirthDateIntentHandler() : base("NetCoreBirthDateIntent")
+        {
+        }
+
+        public override void Process()
+        {
+            try
+            {
+                var numDaysOld = (DateTime.Today - new DateTime(2016, 6, 27)).TotalDays;
+                ResponseEnv.SetOutputSpeechText($"Dot Net Core is {numDaysOld} days old today");
+            }
+            catch (Exception)
+            {
+                ResponseEnv.SetOutputSpeechText("I'm sorry, something went wrong.  Can you try again?");
+            }
+
+        }
+    }
+}
